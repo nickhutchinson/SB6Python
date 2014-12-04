5e1b7976bdb16b361149d2999260707e46f4aa65
Nicholas Hutchinson <nshutchinson@gmail.com>
2014-12-04 20:41:26 +0000

    setup.py: use find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='SB6',
     version='0.1',
-    packages=['sb6'],
+    packages=find_packages('src'),
     package_dir = {'': 'src'},
     install_requires=[
         'Click',
