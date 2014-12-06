22dc28bdb0a431c563e7e9c7065a58b6bb1895c7
Nicholas Hutchinson <nshutchinson@gmail.com>
2014-12-06 18:57:18 +0000

    Tidy directory structure

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
 from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
-import os
-from functools import partial
-
-SB6_ROOT = '/Users/Nick/Downloads/sb6code'
-
-rooted = partial(os.path.join, SB6_ROOT)
 
 exts = [
     Extension(
         'sb6.app',
-        sources=([
+        sources=[
             'src/sb6/app.pyx',
-            'src/sb6/sb6-c.cpp',
-        ] + map(rooted, [
-            'src/sb6/gl3w.c',
-            'src/sb6/sb6.cpp',
-            'src/sb6/sb6ktx.cpp',
-            'src/sb6/sb6object.cpp',
-            'src/sb6/sb6shader.cpp', 
-        ])),
-        include_dirs=[os.path.join(SB6_ROOT, 'include')],
+            'src/sb6/sb6-c/sb6-c.cpp',
+            'third_party/sb6/src/sb6/gl3w.c',
+            'third_party/sb6/src/sb6/sb6.cpp',
+            'third_party/sb6/src/sb6/sb6ktx.cpp',
+            'third_party/sb6/src/sb6/sb6object.cpp',
+            'third_party/sb6/src/sb6/sb6shader.cpp',
+        ],
+        include_dirs=['third_party/sb6/include', 'src/sb6/sb6-c'],
         libraries = ['glfw', ]
     )
 ]
